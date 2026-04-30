63b5bb1c9c57a4fb9fc18da393f259282e91af7c
mfierrojerez <129559858+mfierrojerez@users.noreply.github.com>
2026-04-30 15:28:27 -0400

    Delete Deployment/dist directory

--- Deployment/dist/assets/Configuracion-CpYvTEAn.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CSHb0RNa.js";function i(){return n.jsx("h1",{children:"Configuración"})}export{i as default};
--- Deployment/dist/assets/PublicacionesAdmin-eJRnzP7v.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-CSHb0RNa.js";function r(){return i.jsx("h1",{children:"Administrar Publicaciones"})}export{r as default};
